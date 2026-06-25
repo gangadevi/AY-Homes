83f063e24a429c359ed8e764404670564ee60714
Ganga devi <gangadhiya@gmail.com>
2026-06-25 18:46:57 +0530

    effects and about us added

--- js/index.js
+++ js/index.js
@@ -25,5 +25,51 @@ function initMenu() {
     });
 }
 
+// Counter Animation starts
+
+const counters = document.querySelectorAll('.counter');
+
+const startCounter = (counter) => {
+    const target = +counter.getAttribute('data-target');
+    let count = 0;
+
+    const updateCounter = () => {
+        const increment = Math.max(1, Math.ceil(target / 300));
+
+        if (count < target) {
+            count += increment;
+            counter.innerText = count > target ? target : count;
+            setTimeout(updateCounter, 30); 
+        }else if(target === 10){
+            counter.innerText = target + 'k+';
+        }else {
+            counter.innerText = target + '+';
+        }
+    };
+
+    updateCounter();
+};
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                startCounter(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.5
+    }
+);
+
+counters.forEach((counter) => {
+    observer.observe(counter);
+});
+
+// *************** page animations*************** //
+
+
 loadComponent("header", "header.html");
 loadComponent("footer", "footer.html");
